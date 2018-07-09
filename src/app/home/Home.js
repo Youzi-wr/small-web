88929c03679be283405b53d923979a3d98952932
wangru <1023820943@qq.com>
2018-07-09 17:58:09 +0800

    add cardList

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -7,16 +7,48 @@ const sty = style;
 const { Meta } = Card;
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardData: [{
+                url: 'src/images/home/blu.jpg',
+                title: 'wujin1',
+                description: 'http://localhost:8081/'
+            }, {
+                url: 'src/images/home/moon.jpg',
+                title: 'wujin2',
+                description: 'http://localhost:8081/'
+            }, {
+                url: 'src/images/home/dull.jpg',
+                title: 'wujin3',
+                description: 'http://localhost:8081/'
+            }, {
+                url: 'src/images/home/digita.jpg',
+                title: 'wujin4',
+                description: 'http://localhost:8081/'
+            }, {
+                url: 'src/images/home/hydrogen.jpg',
+                title: 'wujin5',
+                description: 'http://localhost:8081/'
+            }, {
+                url: 'src/images/home/zinc.jpg',
+                title: 'wujin6',
+                description: 'http://localhost:8081/'
+            }] 
+        }
+    }
     render() {
+        const { cardData } = this.state;
+
         return (
             <div className={sty.home}>
                 <div className={sty.headWrap}>
                     <div className={sty.headTopWrap}>
                         <div className={sty.headTop}>
                             <span className={sty.left}>Welcome to Kangyuan</span>
-                            <span className={sty.right}>
+                            {/* <span className={sty.right}>
                                 <a href="javascript:;">换肤</a>
-                            </span>
+                            </span> */}
                         </div>
                     </div>
                     <div className={sty.head}>
@@ -40,16 +72,21 @@ class Home extends React.Component {
                     </div>
                 </div>
                 <div className={sty.cardWrap}>
-                    <Card
-                        hoverable
-                        style={{ width: 240 }}
-                        // cover={<img alt="example" src="src/images/home/moon.jpg" />}
-                    >
-                        <Meta
-                        title="Europe Street beat"
-                        description="www.instagram.com"
-                        />
-                    </Card>
+                    { cardData.map((item, i) => {
+                        return (
+                            <Card
+                                key = {i}
+                                hoverable
+                                style={{ width: 240 }}
+                                cover={<img alt="example" src={item.url}/>}
+                            >
+                                <Meta
+                                title={item.title}
+                                description={item.description}
+                                />
+                            </Card>
+                        )
+                    }) }
                 </div>
             </div>
         );
